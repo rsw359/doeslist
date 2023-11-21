c0b8f467823ca629e473b329e01396e10732af8b
Roger Wells <rsw359@gmail.com>
2023-11-21 15:30:40 +0900

    fixed db errors and migrated

--- doeslist/settings.py
+++ doeslist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'doeslist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER':config('DATABASE_USER'),
+        'PASSWORD':config('DATABASE_PASSWORD'),
+        'HOST':config('DATABASE_HOST', default='localhost'),
+        'PORT':config('DATABASE_PORT', default=5432, cast=int),
+
     }
 }
 
